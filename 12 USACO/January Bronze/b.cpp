71f9deaa0dde08d502ccc88276c44c0c75b7d065
Mauricio Muñoz <a00832956@itesm.mx>
2021-01-26 13:48:58 -0800

    usaco bronze january

---
+++ 12 USACO/January Bronze/b.cpp
@@ -0,0 +1,192 @@
+/*
+    Made by Mau:D
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long int;
+using ld = long double;
+using db = double;
+using str = string;
+
+using pi = pair<int, int>;
+using pl = pair<ll, ll>;
+using pd = pair<db, db>;
+
+using vi = vector<int>;
+using vb = vector<bool>;
+using vl = vector<ll>;
+using vd = vector<db>;
+using vs = vector<str>;
+using vpi = vector<pi>;
+using vpl = vector<pl>;
+using vpd = vector<pd>;
+
+#define mp make_pair
+#define fst first
+#define snd second
+
+#define dbg(x) cout << "[" << (#x) << "]: " << (x) << endl
+#define ln cout << '\n';
+
+#define sz(x) (int)(x).size()
+#define all(x) begin(x), end(x)
+#define rall(x) (x).rbegin(), (x).rend()
+#define sor(x) sort(all(x))
+#define rsz resize
+#define ins insert
+#define ft front()
+#define bk back()
+#define pf push_front
+#define pb push_back
+#define eb emplace_back
+#define lb lower_bound
+#define ub upper_bound
+
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+#define ROF(i, a, b) for (int i = (b)-1; i >= (a); --i)
+#define trav(a, x) for (auto &a : x)
+#define arr array
+
+int MOD = 1e9 + 7;
+const ll INF = 1e18;
+const ld PI = acos((ld)-1);
+const int dx[4] = {1, 0, -1, 0}, dy[4] = {0, 1, 0, -1};
+template <class T>
+using pqg = priority_queue<T, vector<T>, greater<T>>;
+
+constexpr int pct(int x) { return __builtin_popcount(x); }     // # of bits set
+constexpr int bits(int x) { return 31 - __builtin_clz(x); }    // floor(log2(x))
+ll cdiv(ll a, ll b) { return a / b + ((a ^ b) > 0 && a % b); } // divide a by b rounded up
+ll fdiv(ll a, ll b) { return a / b - ((a ^ b) < 0 && a % b); } // divide a by b rounded down
+
+// Debug
+
+void print()
+{
+    cout << '\n';
+}
+
+template <typename First, typename... Strings>
+void print(First argument, const Strings &... rest)
+{
+    cout << argument << " ";
+    print(rest...);
+}
+
+template <typename T>
+void dbga(string nombre, T arr[], int size)
+{
+    cout << '[' << nombre << "]: ";
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << '\n';
+}
+
+template <typename T>
+void dbga(string nombre, vector<T> arr)
+{
+    cout << '[' << nombre << "]: ";
+    for (int i = 0; i < arr.size(); i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << '\n';
+}
+
+template <typename T>
+void printa(T arr[], int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << '\n';
+}
+
+template <typename T>
+void printa(vector<T> arr)
+{
+    for (int i = 0; i < arr.size(); i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << '\n';
+}
+
+// Aqui empieza mi codigo:)
+
+void solve()
+{
+    // Si todos son par, entonces solo se puede hacer un grupo
+    // Si todos son impar, 2, 3, 2
+
+    // 7 Impar
+
+    // 2 Impar, Impar, 2 Impar,
+
+    // Si es una mezcla
+
+    // 6 impar
+    // 1 Par
+
+    // Par, Impar, 2 Impar, Impar, 2 Impar
+
+    int n;
+    cin >> n;
+
+    int impar = 0, par = 0;
+    FOR(i, 0, n)
+    {
+        int d;
+        cin >> d;
+        if (d % 2 != 0)
+            impar++;
+        else
+            par++;
+    }
+
+    int ans = 0;
+    int auxI = impar, auxP = par;
+    FOR(i, 0, n)
+    {
+        impar = auxI, par = auxP;
+        FOR(j, 0, i)
+        {
+            impar -= 2;
+            par++;
+        }
+        if (par == impar)
+            ans = max(impar * 2,ans);
+        else if (par > impar)
+            ans = max(impar * 2 + 1,ans);
+        else
+        {
+            if (impar % 2 == 0)
+                ans = max(par * 2, ans);
+            else
+                ans = max(par * 2 - 1, ans);
+        }
+    }
+
+    cout << ans;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int t;
+    t = 1;
+    while (t--)
+    {
+        solve();
+        // ln ln
+    }
+    return 0;
+}
